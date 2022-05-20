61e83a5231b7848996f8f61cd95a7f3750e843f8
unknown <cami2nov@gmail.com>
2022-05-20 10:28:07 -0500

    + atributos instituciones

--- authApp/models/instituciones.py
+++ authApp/models/instituciones.py
@@ -4,6 +4,18 @@ class Institucion(models.Model):
     nombreInsti = models.CharField(max_length=50)
     descripcionInsti = models.TextField()
     direccionInsti = models.TextField()
+    barrioInsti =  models.TextField()
     correoInsti = models.EmailField(null=True)
     telefonoInsti = models.BigIntegerField(null=True)
+    telefonoFijoInsti = models.BigIntegerField(null=True)
     nit = models.BigIntegerField(primary_key=True)
+    tipoZona = models.CharField(max_length=50,choices=(
+        ('Urbana','Urbana'),
+        ('Rural','Rural')
+    ))
+    sectorInsti = models.CharField(max_length=50,choices=(
+        ('Privado','Privado')
+        ('Oficial','Oficial')
+    ))
+    nombreRector =  models.CharField(max_length=50,default="")
+    
